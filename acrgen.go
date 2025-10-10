7aaf09da89f6409e5526ce0627627a306d11227b
Leonid Shangin <leonid.shangin@math.msu.ru>
2025-10-11 02:21:55 +0300

    Add initial implementation of source data importing

--- acrgen.go
+++ acrgen.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"acrgen/tfp"
 	"fmt"
 	"os"
 )
@@ -12,5 +13,39 @@ func main() {
 		fmt.Println("Запустите программу заново, указав названия трёх  \".txt\" файлов: входного, с существующими словами-кандидатами и выходного")
 		return
 	}
-	// srcFilename, dictFilename, outputFilename := argsWithoutProgName[0], argsWithoutProgName[1], argsWithoutProgName[2]
+	srcFilename := argsWithoutProgName[0]
+
+	_, err := importSrcFromFile(srcFilename)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+}
+
+// #
+// Describes one source file entry (line), that represents a variant for acronym letter, its estimation and decoding (description).
+// #
+type SrcEntry struct {
+	letter     rune
+	estimation int
+	decoding   []rune
+}
+type Src []SrcEntry
+
+// #
+// Parse source data file and import its content.
+// #
+func importSrcFromFile(srcFilename string) (src Src, err error) {
+
+	var parseSrcFileLine tfp.LineParserFunc = func(line string) error {
+		return nil
+	}
+
+	_, err = tfp.ParseFileLineByLine(srcFilename, parseSrcFileLine)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return src, nil
 }
