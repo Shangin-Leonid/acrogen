ac5f632cfd574e1647139564caa95bd6e183c92a
Leonid Shangin <leonid.shangin@math.msu.ru>
2026-06-14 22:15:11 +0300

    Add 'SplitSlice' algorithm

--- algo/slices.go
+++ algo/slices.go
@@ -13,3 +13,45 @@ func ReverseSlice[T any](slice []T) {
 		slice[i], slice[len(slice)-1-i] = slice[len(slice)-1-i], slice[i]
 	}
 }
+
+// # IndRange
+//
+// # TODO:
+//   - docs
+type IndRange struct {
+	Beg int
+	End int
+}
+
+// # SplitSlice
+//
+// # TODO:
+//   - docs
+func SplitSlice[T any](slice []T, nParts int) []IndRange {
+
+	if nParts <= 0 {
+		return []IndRange{}
+	}
+
+	if nParts > len(slice) {
+		nParts = len(slice)
+	}
+
+	minLen := len(slice) / nParts
+	maxLen := minLen + 1
+	nWithMaxLen := len(slice) % nParts
+	nWithMinLen := nParts - nWithMaxLen
+
+	ir := make([]IndRange, 0, nParts)
+	beg := 0
+	for range nWithMaxLen {
+		ir = append(ir, IndRange{beg, beg + maxLen})
+		beg += maxLen
+	}
+	for range nWithMinLen {
+		ir = append(ir, IndRange{beg, beg + minLen})
+		beg += minLen
+	}
+
+	return ir
+}
