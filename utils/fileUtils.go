905e6b799c6a62dab6da51023ebb67656a5f2d87
Leonid Shangin <leonid.shangin@math.msu.ru>
2026-06-15 21:18:08 +0300

    Add tests to 'fileUtils' and improve the code

--- utils/fileUtils.go
+++ utils/fileUtils.go
@@ -3,6 +3,7 @@ package utils /* Utils */
 import (
 	"path"
 	"path/filepath"
+	"unicode/utf8"
 )
 
 // # IsTextFileNameValid checks validness of name of plain text file.
@@ -19,15 +20,24 @@ import (
 //
 //   - Check file existance. If it exists then ask user if he is sure about rewriting content.
 func IsTextFileNameValid(filename string) bool {
+
+	if !utf8.ValidString(filename) {
+		return false
+	}
+
 	ext := path.Ext(filename)
 	if ext != ".txt" {
+		return false
+	}
+
+	if filename == ext {
 		return false
 	}
 
 	return true
 }
 
-// # GetWithoutExt trims file extension from filename.
-func GetWithoutExt(filename string) string {
+// # WithoutExt trims file extension from filename.
+func WithoutExt(filename string) string {
 	return filename[:len(filename)-len(filepath.Ext(filename))]
 }
---
+++ utils/fileUtils_test.go
@@ -0,0 +1,42 @@
+package utils_test
+
+import (
+	"fmt"
+
+	"github.com/Shangin-Leonid/acrogen/utils"
+)
+
+func ExampleIsTextFileNameValid() {
+
+	fmt.Println(utils.IsTextFileNameValid("filename"))
+	fmt.Println(utils.IsTextFileNameValid("filename,txt"))
+	fmt.Println(utils.IsTextFileNameValid("filename.ttt"))
+	fmt.Println(utils.IsTextFileNameValid(".txt"))
+	fmt.Println(utils.IsTextFileNameValid("f.json"))
+
+	fmt.Println(utils.IsTextFileNameValid("filename.txt"))
+	fmt.Println(utils.IsTextFileNameValid("file.ru.txt"))
+
+	// Output:
+	// false
+	// false
+	// false
+	// false
+	// false
+	// true
+	// true
+}
+
+func ExampleWithoutExt() {
+
+	fmt.Println(utils.WithoutExt("filename"))
+	fmt.Println(utils.WithoutExt("filename.txt"))
+	fmt.Println(utils.WithoutExt("f.json"))
+	fmt.Println(utils.WithoutExt("file.ru.md"))
+
+	// Output:
+	// filename
+	// filename
+	// f
+	// file.ru
+}
